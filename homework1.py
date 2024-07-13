7d6cd33dc0aafcdebab137ce9a55133ee0cfd045
JorusBane <JorusBane74@gmail.com>
2024-07-13 15:25:45 +0500

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,11 @@
+example = "Я люблю программирование"
+#program 1
+print(example[0])
+#program 2
+print(example[-1])
+#program 3
+print(example[len(example)//2:-1])
+#program 4
+print(example[::-1])
+#program 5
+print(example[::2])
