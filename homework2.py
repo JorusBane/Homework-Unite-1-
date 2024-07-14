3bec8d2947a51bc39cb43b923b66258de8202a5f
JorusBane <JorusBane74@gmail.com>
2024-07-14 20:29:37 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+count_task_done = 12
+count_hours = 1.5
+name_course = "Python"
+time_1_task = count_hours/count_task_done
+print('Курс:',name_course,'всего задач',count_task_done,
+    'затрачено часов:',count_hours,'среднее время выполнения:',time_1_task, )
