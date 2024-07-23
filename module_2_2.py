09f98d11bebd652c0ce9ff9e67467c09fed4f22d
JorusBane <JorusBane74@gmail.com>
2024-07-23 15:38:00 +0500

    Module_2_2

--- module_2_2.py
+++ module_2_2.py
@@ -1,6 +1,10 @@
-first = 2
-second = 0
-third = 2
+print("Впишите первое число: ")
+first = int(input())
+print("Впишите второе число: ")
+second = int(input())
+print("Впишите третье число: ")
+third = int(input())
+print("Количество одинаковых чисел: ")
 if  first == second == third :
     print(3)
 elif first == second or second == third or third == first:
