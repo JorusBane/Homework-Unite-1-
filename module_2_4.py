bf99b7caab676a767273e73de6389bb65cff950c
JorusBane <JorusBane74@gmail.com>
2024-07-24 08:43:47 +0500

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -2,6 +2,9 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 is_prime = True
+for i in numbers:
+    if i == 1:
+        numbers.remove(i)
 for num in numbers:
     is_prime = True
     for d in range(2, num):
