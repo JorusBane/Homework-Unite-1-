6cd04ccea335df76cccc8674bf2a4a24ba44f601
Owin <JorusBane74@gmail.com>
2024-08-12 04:16:38 +0500

    start

---
+++ Modul_4_(1)/module_4_1.py
@@ -0,0 +1,10 @@
+from True_Math import divide as True_Divide
+from Fake_math import divide as Fake_Divide
+result1 = Fake_Divide(69, 3)
+result2 = Fake_Divide(3, 0)
+result3 = True_Divide(49, 7)
+result4 = True_Divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
