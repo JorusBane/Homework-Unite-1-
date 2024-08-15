8f522dc2421d01d5915a0399f7aa54dd028ade75
Owin <JorusBane74@gmail.com>
2024-08-15 12:09:14 +0500

    homework module 5 task 1

---
+++ Module_5_1.py
@@ -0,0 +1,18 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to(self, floor):
+        if floor < 1 or floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            print(floor)
+
+
+h3 = House( "ЖК 'Эльбрус'", 30)
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h1.go_to(0)
+h2.go_to(10)
+h3.go_to(9)
