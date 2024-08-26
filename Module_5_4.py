ad02e86ebd28b97cee472b115d08f7854e9894e4
JorusBane <JorusBane74@gmail.com>
2024-08-26 17:46:21 +0500

    Update Module_5_4.py
    add methods from Module 5_3

--- Module_5_4.py
+++ Module_5_4.py
@@ -11,6 +11,43 @@ class House:
     def __del__(self):
         print(f"{self.name} снесен, но он останется в истории")
 
+        def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self,   value):
+        self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __radd__(self, value):
+        self.number_of_floors = value + self.number_of_floors
+        return self
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+
+
 
 h1 = House('ЖК Эльбрус', 10)
 print(House.houses_history)
