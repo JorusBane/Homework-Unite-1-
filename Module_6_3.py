1a855c501d4e2b3618bcb977b64278e79b6cedaf
JorusBane <JorusBane74@gmail.com>
2024-08-30 13:06:52 +0500

    Update Module_6_3.py
    fix, make __init__ for create atributs for every object of class

--- Module_6_3.py
+++ Module_6_3.py
@@ -1,23 +1,51 @@
 class Horse:
-    x_distance = 0
-    sound = ('Frrr')
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = "Frrr"
+        super().__init__()
+
     def run(self, dx):
-        self.x_distance = self.x_distance + dx
+        self.x_distance += dx
+
 
 class Eagle:
-    y_distance = 0
-    sound = "i train, sleep and repeat"
-    def fly(self, dy):
-        self.y_distance = self.y_distance + dy
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = "i train, sleep and repeat"
+        super().__init__()
 
-class Pegasus (Horse, Eagle):
+    def fly(self, dy):
+        self.y_distance += dy
+class Pegasus(Horse, Eagle):
+    def __init__(self):
+        super().__init__()
     def move(self, dx, dy):
         super().fly(dy)
         super().run(dx)
+
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+
+    def voice(self):
+        print(self.sound)
+
+class Hippo(Eagle, Horse):
+    def __init__(self):
+        super().__init__()
+    def move(self, dx, dy):
+        super().fly(dy)
+        super().run(dx)
+
     def get_pos(self):
         return (self.x_distance, self.y_distance)
+
     def voice(self):
-        print(Eagle.sound)
+        print(self.sound)
+
+
+
+
+
 
 print(Pegasus.mro())
 print(Eagle.mro())
@@ -30,3 +58,13 @@ p1.move(-5, 20)
 print(p1.get_pos())
 
 p1.voice()
+
+h1 = Hippo()
+print(Hippo.mro())
+print(h1.get_pos())
+h1.move(8, 90)
+print(h1.get_pos())
+h1.move(-7, 70)
+print(h1.get_pos())
+
+h1.voice()
