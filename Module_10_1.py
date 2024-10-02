060f6d5d5041a0adb99a70f6c515dc9d69194742
JorusBane <JorusBane74@gmail.com>
2024-10-02 12:34:47 +0500

    Add files via upload

---
+++ Module_10_1.py
@@ -0,0 +1,42 @@
+from time import sleep
+from threading import Thread
+from datetime import datetime
+
+
+def write_words(word_count, file_name):
+    with (open(file_name, 'w', encoding='utf-8') as file):
+        file.writelines(f'Какое-то слово {num}\n ' for num in range(word_count+1))
+        sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+
+time_start = datetime.now()
+write_words(10, "example1.txt")
+write_words(30, "example2.txt")
+write_words(200, "example3.txt")
+write_words(100, "example4.txt")
+time_end = datetime.now()
+time_res = time_end - time_start
+print(time_res)
+
+
+thr_first = Thread(target=write_words(10, "example5.txt"))
+thr_second = Thread(target=write_words(30, "example6.txt"))
+thr_third = Thread(target=write_words(200, "example7.txt"))
+thr_four = Thread(target=write_words(100, "example8.txt"))
+
+time_start = datetime.now()
+
+thr_first.start()
+thr_second.start()
+thr_third.start()
+thr_four.start()
+
+thr_first.join()
+thr_second.join()
+thr_third.join()
+thr_four.join()
+
+time_end = datetime.now()
+time_res = time_end - time_start
+print(time_res)
