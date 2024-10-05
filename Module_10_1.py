f9613639b35dff8d408464bac8e0bb4705eb7c09
JorusBane <JorusBane74@gmail.com>
2024-10-05 13:06:39 +0500

    Update Module_10_1.py
    Теперь задержка после каждой строчки.
    При создании потока передаю название функции и аргументы отдельно, не вызывая функцию в target =

--- Module_10_1.py
+++ Module_10_1.py
@@ -5,8 +5,9 @@ from datetime import datetime
 
 def write_words(word_count, file_name):
     with (open(file_name, 'w', encoding='utf-8') as file):
-        file.writelines(f'Какое-то слово {num}\n ' for num in range(word_count+1))
-        sleep(0.1)
+        for num in range(word_count + 1):
+            file.writelines(f'Какое-то слово {num}\n ')
+            sleep(0.1)
     print(f"Завершилась запись в файл {file_name}")
 
 
@@ -20,10 +21,10 @@ time_res = time_end - time_start
 print(time_res)
 
 
-thr_first = Thread(target=write_words(10, "example5.txt"))
-thr_second = Thread(target=write_words(30, "example6.txt"))
-thr_third = Thread(target=write_words(200, "example7.txt"))
-thr_four = Thread(target=write_words(100, "example8.txt"))
+thr_first = Thread(target=write_words, args=(10, "example5.txt"))
+thr_second = Thread(target=write_words, args=(30, "example6.txt"))
+thr_third = Thread(target=write_words, args=(200, "example7.txt"))
+thr_four = Thread(target=write_words, args=(100, "example8.txt"))
 
 time_start = datetime.now()
 
