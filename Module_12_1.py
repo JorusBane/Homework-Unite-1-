5517a15f45e4e7e13062025b0fe913e80b2c11c1
JorusBane <JorusBane74@gmail.com>
2024-10-26 17:10:59 +0500

    Update Module_12_1.py

--- Module_12_1.py
+++ Module_12_1.py
@@ -2,11 +2,13 @@ import Runner
 import unittest
 
 class runner_test(unittest.TestCase):
+    
     def test_run(self):
         unit = Runner.Runner("Ruslan")
         for _ in range(10):
             unit.run()
         self.assertEqual(unit.distance, 100)
+        
     def test_walk(self):
         unit = Runner.Runner("Ruslan")
         for _ in range(10):
