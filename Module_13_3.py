1abccfbed72ee148332160b6e4148c8e06d5cf9a
JorusBane <JorusBane74@gmail.com>
2024-11-08 20:10:52 +0500

    Update Module_13_3.py

--- Module_13_3.py
+++ Module_13_3.py
@@ -7,17 +7,6 @@ api = ""
 bot = Bot(token= api)
 dp = Dispatcher(bot, storage= MemoryStorage())
 
-WishesList = "File1"
-with (open(WishesList, encoding='utf-8') as file):
-    count = 0
-    file_att = []
-    for line in file:
-        file_att.append(line)
-        count += 1
-
-counter = random.randint(0, count)
-
-
 @dp.message_handler(commands= ["start"])
 async def start(message):
     await message.answer('Привет! Я бот помогающий твоему здоровью.')
