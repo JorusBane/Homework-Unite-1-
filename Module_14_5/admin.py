18ed8061b5d94d7f2f01286d26b8dc75791431d4
JorusBane <JorusBane74@gmail.com>
2024-11-26 21:43:58 +0500

    Update admin.py

--- Module_14_5/admin.py
+++ Module_14_5/admin.py
@@ -8,7 +8,7 @@ cursor = connection.cursor()
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Users(
-id INTEGER PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 username TEXT NOT NULL,
 email TEXT NOT NULL,
 age INTEGER,
@@ -20,7 +20,8 @@ balance INTEGER NOT NULL
 def add_user(username, email, age):
     check_user = cursor.execute("SELECT * FROM Users WHERE username=?", (username, ))
     if check_user.fetchone() is None:
-        cursor.execute("INSERT INTO Users VALUES( ?, ?, ?, ?)", (f"{username}", f"{email}", f"{age}", "1000"))
+        cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES( ?, ?, ?, ?)", 
+                       (f"{username}", f"{email}", f"{age}", 1000))
     connection.commit()
 
 def is_included(username):
