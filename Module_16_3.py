200554467e01006abcc3a54fed067802ed6fe559
JorusBane <JorusBane74@gmail.com>
2024-12-23 21:26:16 +0500

    Update Module_16_3.py
    Добавил валидацию и сделал обработку пользователей заместо абстрактных сообщений

--- Module_16_3.py
+++ Module_16_3.py
@@ -1,34 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import  Annotated
 
 app = FastAPI()
 
-messages_db = {"0": "First post in FastApi"}
+users = {"1": "Имя: Иван, Возраст: 18"}
 
 @app.get("/")
-async  def get_all_messages() -> dict:
-    return messages_db
+async  def get_all_users() -> dict:
+    return users
 
-@app.get("/message/{message_id}")
-async def get_message(message_id: str) -> dict:
-    return messages_db[message_id]
+@app.post("/{username}/{age}")
+async def create_user(username: str = Path(min_length=3, max_length=15, description="Введите имя пользователя", example="Иван"),
+                      age: int = Path(ge=18, le=120, description="Введите возраст пользователя", example=18) ) -> str:
+    current_index = str(int(max(users, key=int)) + 1)
+    users[current_index] = f"Имя {username}, возраст {age}"
+    return "Пользователь добавлен!"
 
-@app.post("/message")
-async def create_message(message: str) -> str:
-    current_index = str(int(max(messages_db, key=int)) + 1)
-    messages_db[current_index] = message
-    return "Данные добавлены!"
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(user_id: int = Path(ge=0, le=1000, description="Введите номер в списке", example=3),
+                      username: str = Path(min_length=3, max_length=15, description="Введите имя пользователя", example="Иван"),
+                      age:  int = Path(ge=18, le=120, description="Введите возраст", example=18)) -> str:
+    users[str(user_id)] =  f"Имя {username}, возраст {age}"
+    return "Данные о пользователе обновлены!"
 
-@app.put("/message/{message_id}")
-async def update_message(message_id: str, message: str) -> str:
-    messages_db[message_id] = message
-    return "Данные обновлены!"
+@app.delete("/users/{user_id}")
+async  def delete_user(user_id: str = Path(ge=0, le=1000, description="Введите номер в списке", example=2)) -> str:
+    users.pop(user_id)
+    return f"Пользователь {user_id} были удалены."
 
-@app.delete("/message/{message_id}")
-async  def delete_massage(message_id: str) -> str:
-    messages_db.pop(message_id)
-    return f"Данные {message_id} были удалены."
-
-app.delete("/")
-async  def delete_all_messages() -> str:
-    messages_db.clear()
+@app.delete("/")
+async  def delete_all_users() -> str:
+    users.clear()
     return "Обнуление"
